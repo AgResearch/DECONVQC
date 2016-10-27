cec537e9a1791656ded1f0b0ea0737b1b500220c
afmcc <alanfmcculloch@gmail.com>
2016-10-27 13:33:38 +1300

    added subjectname to keyfile database table, and updated keyfile extract scripts

--- database/listDBKeyfile.sh
+++ database/listDBKeyfile.sh
@@ -6,10 +6,11 @@ function get_opts() {
 help_text="\n
  this scripts extracts a keyfile from the database \n
 
- listKeyfile.sh -s sample_name  [-v Tassel version] \n
+ listKeyfile.sh -s sample_name  [-v Tassel version] [-t extract template]\n
 \n
  e.g.\n
  listDBKeyfile.sh -s SQ0032 \n
+ listDBKeyfile.sh -s SQ0002 -t all \n
  listDBKeyfile.sh -s SQ0032 -v 5 \n
  listDBKeyfile.sh -s SQ2534 -v 5 -f C6K0YANXX \n
 
@@ -17,8 +18,9 @@ help_text="\n
 
 TASSEL_VERSION=3
 FLOWCELL=""
+TEMPLATE="default"
 
-while getopts ":nhv:s:k:c:f:" opt; do
+while getopts ":nhv:s:k:c:f:t:" opt; do
   case $opt in
     s)
       SAMPLE=$OPTARG
@@ -28,6 +30,9 @@ while getopts ":nhv:s:k:c:f:" opt; do
       ;;
     f)
       FLOWCELL=$OPTARG
+      ;;
+    t)
+      TEMPLATE=$OPTARG
       ;;
     h)
       echo -e $help_text
@@ -60,6 +65,10 @@ function check_opts() {
       echo "Tassel version should be 3 or 5"
       exit 1
    fi
+   if [[ $TEMPLATE != "default" && $TEMPLATE != "all" ]]; then
+      echo "template should be default or all"
+      exit 1
+   fi
 
 }
 
@@ -72,15 +81,15 @@ check_opts
 ############ process the extract ###############
 if [ $TASSEL_VERSION  == "3" ]; then
    if [ -z $FLOWCELL ]; then
-      psql -q -U gbs -d agrbrdf -h invincible -v keyfilename=\'$SAMPLE\' -f $GBS_BIN/database/extractKeyfile.psql 
+      psql -q -U gbs -d agrbrdf -h invincible -v keyfilename=\'$SAMPLE\' -f $GBS_BIN/database/extractKeyfile_${TEMPLATE}.psql 
    else
-      psql -q -U gbs -d agrbrdf -h invincible -v keyfilename=\'$SAMPLE\' -f $GBS_BIN/database/extractKeyfile.psql | egrep -i \($FLOWCELL\|flowcell\)
+      psql -q -U gbs -d agrbrdf -h invincible -v keyfilename=\'$SAMPLE\' -f $GBS_BIN/database/extractKeyfile_${TEMPLATE}.psql | egrep -i \($FLOWCELL\|flowcell\)
    fi
 else
    if [ -z $FLOWCELL ]; then
-      psql -q -U gbs -d agrbrdf -h invincible -v keyfilename=\'$SAMPLE\' -f  $GBS_BIN/database/extractKeyfile5.psql   
+      psql -q -U gbs -d agrbrdf -h invincible -v keyfilename=\'$SAMPLE\' -f  $GBS_BIN/database/extractKeyfile5_${TEMPLATE}.psql   
    else
-      psql -q -U gbs -d agrbrdf -h invincible -v keyfilename=\'$SAMPLE\' -f  $GBS_BIN/database/extractKeyfile5.psql | egrep -i \($FLOWCELL\|flowcell\)  
+      psql -q -U gbs -d agrbrdf -h invincible -v keyfilename=\'$SAMPLE\' -f  $GBS_BIN/database/extractKeyfile5_${TEMPLATE}.psql | egrep -i \($FLOWCELL\|flowcell\)  
    fi
 fi
 
