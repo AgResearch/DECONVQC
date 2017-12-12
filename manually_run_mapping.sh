3c2015254cab4950e6ca7e2e1b88821e5cba1c12
afmcc <alanfmcculloch@gmail.com>
2017-12-13 10:11:28 +1300

    added script for manually running mapping preview

---
+++ manually_run_mapping.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# sometimes need to run the mapping preview "manually"
+# - define RUN as below and then execute
+#
+
+#RUN=171208_D00390_0334_BCBE4WANXX
+RUN=171121_D00390_0333_ACBG2FANXX
+DIR=/dataset/hiseq/scratch/postprocessing/${RUN}.processed/mapping_preview_in_progress 
+
+
+export HPC_RESOURCE=local
+mkdir -p $DIR
+module load samtools14_env/1.0/1.0 
+cd $DIR ; $GBS_BIN/run_mapping_preview.sh $RUN $DIR  > $DIR/run_mapping_preview.rerun.log 2>&1
+
+
